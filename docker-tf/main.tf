72d2b1d111df55ae775943e3f58fe8311209ec99
Ilan Arlin <ilanarlin@gmail.com>
2024-10-28 19:40:05 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,27 @@
+# by Ilan Arlin
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+#This is a resource 
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
